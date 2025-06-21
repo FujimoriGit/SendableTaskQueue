4b6ba05634e25f9fed9102aeb3a6a7dcc5960161
Daiki Fujimori <G016C1102@outlook.jp>
2025-06-21 14:11:29 +0900

    Initial Commit

---
+++ SendableTaskQueue/SendableTaskQueue.swift
@@ -0,0 +1,10 @@
+//
+//  SendableTaskQueue.swift
+//  SendableTaskQueue
+//  
+//  Created by Daiki Fujimori on 2025/06/21
+//  
+
+
+import Foundation
+
---
+++ SendableTaskQueueTests/SendableTaskQueueTests.swift
@@ -0,0 +1,18 @@
+//
+//  SendableTaskQueueTests.swift
+//  SendableTaskQueueTests
+//  
+//  Created by Daiki Fujimori on 2025/06/21
+//  
+
+
+import Testing
+@testable import SendableTaskQueue
+
+struct SendableTaskQueueTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
